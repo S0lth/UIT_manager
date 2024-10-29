8d8304965fe8aa4f5c98e16edc78eef2f2a23105
Gerard Alex <alex.gerard@student.hers.be>
2024-10-29 18:45:17 +0100

    feat: agent collect users

---
+++ Code/UITManagerAgent/DataCollectors/UsersCollector.cs
@@ -0,0 +1,29 @@
+﻿namespace UITManagerAgent.DataCollectors;
+using System.DirectoryServices;
+
+public class UserCollector : DataCollector
+{
+    public Information Collect()
+    {
+        UsersInformation users = new UsersInformation();
+        
+        try {
+            using (DirectoryEntry localMachine = new DirectoryEntry("WinNT://" + Environment.MachineName))
+            {
+                foreach (DirectoryEntry child in localMachine.Children)
+                {
+                    if (child.SchemaClassName == "User")
+                    {
+                        if (child != null) users.usersList.Add(child.Name);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erreur lors de la récupération des utilisateurs : " + ex.Message);
+        }
+
+        return users;
+    }
+}
