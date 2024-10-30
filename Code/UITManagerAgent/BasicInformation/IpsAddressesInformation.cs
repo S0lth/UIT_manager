bff7f8abfe52604deada6dca1fa89861a02b6cd0
Dorian H <dorian.hesbois@student.hers.be>
2024-10-30 16:06:24 +0100

    feat: agent collect IPS informations
    Add of classes IpsAddressesInformation, IpsAddressesCollector and editing Program.cs

---
+++ Code/UITManagerAgent/BasicInformation/IpsAddressesInformation.cs
@@ -0,0 +1,9 @@
+﻿namespace UITManagerAgent.DataCollectors;
+
+public class IpsAddressesInformation : Information
+{
+    public List<string> IPSList = new();
+
+    public override string ToString() => $"{string.Join(", ", IPSList)}";
+
+}
