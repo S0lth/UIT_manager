beed475650ec134c131860641220436ead80287d
Dorian H <dorian.hesbois@student.hers.be>
2024-10-31 22:16:56 +0100

    fix: replace brackets by ; in IpsAdressesCollector
    Related to #11

--- Code/UITManagerAgent/DataCollectors/IpsAddressesCollector.cs
+++ Code/UITManagerAgent/DataCollectors/IpsAddressesCollector.cs
@@ -2,9 +2,14 @@ using System.Net;
 using System.Net.Sockets;
 using UITManagerAgent.BasicInformation;
 
-namespace UITManagerAgent.DataCollectors {
+namespace UITManagerAgent.DataCollectors; 
+/// <summary>
+/// Class responsible for collecting IP addresses.
+/// </summary>
+public class IpsAddressesCollector : DataCollector
+{
     /// <summary>
-    ///     Class responsible for collecting IP addresses.
+    /// Collects the IP addresses from the local system.
     /// </summary>
     /// <returns>
     /// An <see cref="IpsAddressesInformation"/> object containing the collected IP addresses.
@@ -30,8 +35,6 @@ namespace UITManagerAgent.DataCollectors {
                     ipsAddressesInformation.GetIpsList().Add(ip.ToString());
                 }
             }
-
-            return ipsAddressesInformation;
         }
         catch (SocketException socketException)
         {
