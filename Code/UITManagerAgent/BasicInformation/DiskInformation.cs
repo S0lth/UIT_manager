be6205d43695897a5d53d9837fc5803f301eef07
Mathis Spronck <mathis.spronck@student.hers.be>
2024-11-01 15:23:36 +0100

    refactor(agent): modif methode DiskInformation and add documentation in DiskCollector and DiskInformation

--- Code/UITManagerAgent/BasicInformation/DiskInformation.cs
+++ Code/UITManagerAgent/BasicInformation/DiskInformation.cs
@@ -1,7 +1,11 @@
-﻿using UITManagerAgent.DataCollectors;
+﻿using System.Management;
+using UITManagerAgent.DataCollectors;
 
 namespace UITManagerAgent.BasicInformation
 {
+    /// <summary>
+    /// Provides information on all disk names, total storage capacity, free storage capacity, and number of disks
+    /// </summary>
     public class DiskInformation : Information
     {
         private List<string> _disksName = new();
@@ -9,26 +13,48 @@ namespace UITManagerAgent.BasicInformation
         private List<long> _disksFreeSize = new();
         private int _numberDisk;
 
-        public List<string> getDiskName()
-        {
-            return _disksName;
+        /// <summary>
+        /// accessors of the disksName field
+        /// </summary>
+        public List<string>? DisksName {
+            get => _disksName;
+            set => _disksName = value;
         }
 
-        public List<long> getDiskTotalSize()
-        {
-            return _diskTotalSize;
+        /// <summary>
+        /// accessors of the diskTotalSize field
+        /// </summary>
+        public List<long>? DiskTotalSize {
+            get => _diskTotalSize;
+            set => _diskTotalSize = value;
         }
 
-        public List<long> getDisksFreeSizes()
-        {
-            return _disksFreeSize;
+
+        /// <summary>
+        /// accessors of the disksFreeSize field
+        /// </summary>
+        public List<long>? DisksFreeSize {
+            get => _disksFreeSize;
+            set => _disksFreeSize = value;
         }
 
-        public void setNumberDisk(int numberDisk)
-        {
-            _numberDisk = numberDisk;
+        /// <summary>
+        /// accessors of the numberDisk field
+        /// </summary>
+        public int NumberDisk {
+            get => _numberDisk;
+            set => _numberDisk = value;
         }
 
+
+        /// <summary>
+        /// Returns a formatted string representation of the disk information, 
+        /// including disk names, total sizes, free space, and the number of disks.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string"/> representing the disk information in a readable format.
+        /// Each property is presented on a new line with appropriate labels.
+        /// </returns>
         public override string ToString()
         {
             return string.Join("Nom disque : ", _disksName) + "\n" + string.Join("Total size : ", _diskTotalSize) + "\n" + string.Join("Free size : ", _disksFreeSize) + "\n" +
