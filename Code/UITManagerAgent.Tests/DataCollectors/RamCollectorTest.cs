87d1c50379669b7b74e498fade5c94e074651aa1
Pau <pauline.guerisse@student.hers.be>
2024-11-01 22:31:17 +0100

    refactor : add annotation
    related #10

--- Code/UITManagerAgent.Tests/DataCollectors/RamCollectorTest.cs
+++ Code/UITManagerAgent.Tests/DataCollectors/RamCollectorTest.cs
@@ -1,4 +1,5 @@
-﻿using UITManagerAgent.BasicInformation;
+﻿using System.Runtime.Versioning;
+using UITManagerAgent.BasicInformation;
 using UITManagerAgent.DataCollectors;
 
 namespace UITManagerAgent.Tests.DataCollectors;
@@ -7,17 +8,16 @@ namespace UITManagerAgent.Tests.DataCollectors;
 /// Contains unit tests for the <see cref="RamCollector"/> class.
 /// </summary>
 [TestClass]
-public class RamCollectorTest
-{
-    private RamCollector _ramCollector;
-    private RamInformation _ramInformation;
+[SupportedOSPlatform("windows")]
+public class RamCollectorTest {
+    private RamCollector? _ramCollector;
+    private RamInformation? _ramInformation;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="RamCollector"/> class and the <see cref="RamInformation"/> class before each test.
     /// </summary>
     [TestInitialize]
-    public void Setup()
-    {
+    public void Setup() {
         _ramCollector = new RamCollector();
         _ramInformation = new RamInformation();
     }
@@ -26,28 +26,28 @@ public class RamCollectorTest
     /// Tests the <see cref="RamCollector.Collect"/> method to ensure it sets memory values correctly.
     /// </summary>
     [TestMethod]
-    public void Test_Collect_SetsMemoryValuesCorrectly()
-    {
-        _ramInformation = (RamInformation)_ramCollector.Collect();
-
-        Assert.IsNotNull(_ramInformation);
-        Assert.IsTrue(_ramInformation.TotalMemory > 0, "Total memory should be greater than 0");
-        Assert.IsTrue(_ramInformation.FreeMemory >= 0, "Free memory should be non-negative");
-        Assert.IsTrue(_ramInformation.UsedMemory >= 0, "Used memory should be non-negative");
+    public void Test_Collect_SetsMemoryValuesCorrectly() {
+        if (_ramCollector != null) {
+            _ramInformation = (RamInformation)_ramCollector.Collect()!;
+            Assert.IsNotNull(_ramInformation);
+            Assert.IsTrue(_ramInformation.TotalMemory > 0, "Total memory should be greater than 0");
+            Assert.IsTrue(_ramInformation.FreeMemory >= 0, "Free memory should be non-negative");
+            Assert.IsTrue(_ramInformation.UsedMemory >= 0, "Used memory should be non-negative");
+        }
     }
 
     /// <summary>
     /// Tests the <see cref="RamCollector.Collect"/> method to ensure it calculates used memory correctly.
     /// </summary>
     [TestMethod]
-    public void Test_Collect_CalculatesUsedMemoryCorrectly()
-    {
-        _ramInformation = (RamInformation)_ramCollector.Collect();
+    public void Test_Collect_CalculatesUsedMemoryCorrectly() {
+        if (_ramCollector != null) {
+            _ramInformation = (RamInformation)_ramCollector.Collect();
 
-        Assert.IsTrue(_ramInformation.UsedMemory <= _ramInformation.TotalMemory,
-            "Used memory cannot exceed total memory");
-        Assert.IsTrue(
-            _ramInformation.FreeMemory + _ramInformation.UsedMemory <= _ramInformation.TotalMemory,
-            "Free memory + Used memory should not exceed Total memory");
+            Assert.IsTrue(_ramInformation.UsedMemory <= _ramInformation.TotalMemory,
+                "Used memory cannot exceed total memory");
+            Assert.IsTrue(_ramInformation.FreeMemory + _ramInformation.UsedMemory <= _ramInformation.TotalMemory,
+                "Free memory + Used memory should not exceed Total memory");
+        }
     }
 }
