a9bf95d0b1171c4f12c70a72e4b81259c7a22064
Florentin <florentin.wuidar@student.hers.be>
2024-11-03 15:30:23 +0100

    feat: agent collect directX version

---
+++ Code/UITManagerAgent/DataCollectors/DirectXCollector.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using UITManagerAgent.BasicInformation;
+
+namespace UITManagerAgent.DataCollectors;
+/// <summary>
+/// Collects information about the DirectX version installed on the system.
+/// </summary>
+public class DirectXCollector : DataCollector {
+    /// <summary>
+    /// Collects the DirectX version information from the system using the DirectX Diagnostic Tool (dxdiag).
+    /// </summary>
+    /// <returns>
+    /// A <see cref="DirectXInformation"/> object containing the DirectX version.
+    /// If the DirectX version cannot be retrieved, the object will contain: DirectX not found.
+    /// </returns>
+    public Information Collect() {
+
+        DirectXInformation directXInformation = new DirectXInformation();
+
+        try {
+            ProcessStartInfo startInfo = new ProcessStartInfo("dxdiag.exe") {
+                Arguments = "/t dxdiag.txt",
+                UseShellExecute = false,
+                RedirectStandardOutput = false,
+                CreateNoWindow = true
+            };
+
+            Process? dxDiagProcess = Process.Start(startInfo);
+            dxDiagProcess?.WaitForExit();
+
+            string dxDiagOutput = System.IO.File.ReadAllText("dxdiag.txt");
+
+            System.IO.File.Delete("dxdiag.txt");
+
+            Match match = Regex.Match(dxDiagOutput, @"DirectX Version:\s*DirectX\s*(\d+)");
+            if (match.Success) {
+                directXInformation.DirectX = "DirectX " + match.Groups[1].Value;
+            }
+            else {
+                directXInformation.DirectX = "DirectX not found";
+            }
+        }
+        catch (Exception ex) {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        return directXInformation;
+
+    }
+}
+
