97b1b9c06b77c21d5a40a32acb75b319f276bef2
Florentin <florentin.wuidar@student.hers.be>
2024-11-12 16:48:02 +0100

    add ToJson method in DirectXInformation & add Test

---
+++ Code/UITManagerAgent.Tests/BasicInformation/DirectXTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.Versioning;
+using UITManagerAgent.BasicInformation;
+using UITManagerAgent.DataCollectors;
+
+namespace UITManagerAgent.Tests.BasicInformation;
+
+/// <summary>
+/// This class contains the Unit Tests for method <see cref="DirectXInformation.ToJson"/>
+/// </summary>
+[TestClass]
+public class DirectXTest {
+
+    private DirectXInformation? _directXInformation;
+
+    /// <summary>
+    /// Initialize a new instance of the <see cref="DirectXInformation"/> class before each test.
+    /// </summary>
+    [TestInitialize]
+    public void Setup() {
+        _directXInformation = new();
+    }
+
+    /// <summary>
+    /// Test method to check if the method <see cref="DirectXInformation.ToJson"/>
+    /// returns the correct format with define value
+    /// </summary>
+    [TestMethod]
+    public void ToJson_ShouldReturnValidJson_WhenDirectXIsManuallySet() {
+        if (_directXInformation != null) {
+            _directXInformation.DirectX = "DirectX 12";
+            string json = _directXInformation.ToJson();
+            string expected = "{\"DirectX\":\"DirectX 12\"}";
+            StringAssert.Contains(expected, json);
+        }
+        else {
+            Assert.Fail("DirectX cannot be null");
+        }
+    }
+
+    /// <summary>
+    /// Test method to check if the method <see cref="DirectXInformation.ToJson"/>
+    /// returns the correct format with real value
+    /// </summary>
+    [TestMethod]
+    [SupportedOSPlatform("windows")]
+    public void ToJson_ShouldReturnValidJson_WithActualDirectX() {
+        if (_directXInformation != null) {
+            DirectXCollector directXCollector = new();
+            _directXInformation = (DirectXInformation)directXCollector.Collect();
+            string json = _directXInformation.ToJson();
+            string expected = $"{{\"DirectX\":\"{_directXInformation.DirectX}\"}}";
+            StringAssert.Contains(json, expected);
+        }
+    }
+
+}
