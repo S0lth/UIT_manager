baa32b6e5019809c50041d9827e23a55cfbda8a9
GermainRigaux <germain.rigaux@student.hers.be>
2024-11-18 16:12:27 +0100

    feat: creation of poplate.cs

---
+++ Code/UITManagerWebServer/Data/Populate.cs
@@ -0,0 +1,136 @@
+using UITManagerWebServer.Models;
+using Microsoft.EntityFrameworkCore;
+using UITManagerWebServer.Data;
+
+
+public static class Populate {
+    public static void Initialize(IServiceProvider serviceProvider) {
+        using var context = new ApplicationDbContext(
+            serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>());
+
+        // Vérifiez si la base de données est déjà peuplée
+        if (context.Machines.Any() || context.NormGroups.Any()) {
+            context.Alarms.RemoveRange(context.Alarms);
+            context.Notes.RemoveRange(context.Notes);
+            context.Norms.RemoveRange(context.Norms);
+            context.NormGroups.RemoveRange(context.NormGroups);
+            context.Machines.RemoveRange(context.Machines);
+
+            context.SaveChanges();
+            Console.WriteLine("Database cleared successfully.");
+        }
+
+        // Créer des NormGroups
+        var normGroups = new List<NormGroup> {
+            new NormGroup {
+                Name = "Obsolete operating system" ,
+                Priority = 8,
+                Severity = SeverityLevel.Critical,
+                Norms = new List<Norm> { new Norm { Name = "Windows 10 detected" } }
+            },
+            new NormGroup {
+                Name = "Storage exceded",
+                Priority = 4,
+                Severity = SeverityLevel.High,
+                Norms = new List<Norm> { new Norm { Name = "Storage over than 80%" } }
+            },
+            new NormGroup {
+                Name = "Storage exceded",
+                Priority = 1,
+                Severity = SeverityLevel.Low,
+                Norms = new List<Norm> { new Norm { Name = "Storage over than 30%" } }
+            }
+        };
+        context.NormGroups.AddRange(normGroups);
+
+// Créer des Machines
+        var machines = new List<Machine> {
+            new Machine { Name = "ThinkPad de Germain" },
+            new Machine { Name = "ThinkPad de Alex" },
+            new Machine { Name = "Legion de Mathis" },
+            new Machine { Name = "Acer de Pauline" },
+            new Machine { Name = "VivoBook de Romain" },
+            new Machine { Name = "ThinkBook de Dorain" },
+            new Machine { Name = "Laptop de Florentin" },
+            new Machine { Name = "Desktop-AAAAAAA" },
+            new Machine { Name = "Desktop-BBBBBBB" },
+            new Machine { Name = "Desktop-CCCCCCC" },
+
+        };
+        context.Machines.AddRange(machines);
+
+// Créer des Alarms pour certaines machines
+        var alarms = new List<Alarm> {
+            new Alarm {
+                Status = AlarmStatus.New,
+                TriggeredAt = DateTime.UtcNow,
+                Description = "The Windows version in not up to date",
+                Machine = machines[0],
+                NormGroup = normGroups[0]
+            },
+            new Alarm {
+                Status = AlarmStatus.Acknowledged,
+                TriggeredAt = DateTime.UtcNow.AddHours(-2),
+                Description = "Storage capacity is under 20% left",
+                Machine = machines[1],
+                NormGroup = normGroups[1]
+            },
+            new Alarm {
+                Status = AlarmStatus.Acknowledged,
+                TriggeredAt = DateTime.UtcNow.AddHours(-2),
+                Description = "Storage capacity is under 20% left",
+                Machine = machines[5],
+                NormGroup = normGroups[1]
+            },
+            new Alarm {
+                Status = AlarmStatus.New,
+                TriggeredAt = DateTime.UtcNow,
+                Description = "The Windows version in not up to date",
+                Machine = machines[5],
+                NormGroup = normGroups[0]
+            },
+            new Alarm {
+                Status = AlarmStatus.New,
+                TriggeredAt = DateTime.UtcNow,
+                Description = "The Windows version in not up to date",
+                Machine = machines[6],
+                NormGroup = normGroups[0]
+            },
+
+        };
+        context.Alarms.AddRange(alarms);
+
+// Créer des Notes pour certaines machines
+        var notes = new List<Note> {
+
+            new Note {
+                Content = "check OS everything is ok because is on linux",
+                CreatedAt = DateTime.UtcNow.AddHours(-1),
+                Machine = machines[0],
+                IsSolution = true
+            },
+            new Note {
+                Content = "routine maintenance",
+                CreatedAt = DateTime.UtcNow.AddHours(-1),
+                Machine = machines[4],
+                IsSolution = false
+            },
+            new Note {
+                Content = "routine maintenance",
+                CreatedAt = DateTime.UtcNow.AddHours(-1),
+                Machine = machines[5],
+                IsSolution = false
+            },
+            new Note {
+                Content = "routine maintenance",
+                CreatedAt = DateTime.UtcNow.AddHours(-1),
+                Machine = machines[6],
+                IsSolution = false
+            }
+        };
+        context.Notes.AddRange(notes);
+
+// Enregistrer les modifications
+        context.SaveChanges();
+    }
+}
