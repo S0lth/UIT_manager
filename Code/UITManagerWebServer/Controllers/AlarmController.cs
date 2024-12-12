0f6fe032b717e5dcda1e3d61c401992af94a8229
Pau <pauline.guerisse@student.hers.be>
2024-12-12 21:49:02 +0100

    fix(authorization) : verify verification
    verify AlarmController
    
    close #150

--- Code/UITManagerWebServer/Controllers/AlarmController.cs
+++ Code/UITManagerWebServer/Controllers/AlarmController.cs
@@ -1,9 +1,5 @@
 using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
